f602bbc9cb031db0990c7a8cfe6859b5ec14e76d
kcymax0895 <kcymax0895@gmail.com>
2026-02-27 14:49:35 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
